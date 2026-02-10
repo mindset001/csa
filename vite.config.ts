86c6e4f3d4622b6d5de1400ab06f79d83941ce40
mindset001 <shittumoshoodademola@gmail.com>
2026-02-10 14:47:28 +0100

    'new'

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,17 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      preview: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      build: {
+        rollupOptions: {
+          output: {
+            manualChunks: undefined,
+          },
+        },
+      },
     };
 });
